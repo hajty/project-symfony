ec5017c6552169bdca21fb9c30361a9b9ebbf58f
Bartosz Miłosierny <milosierny.bartosz@gmail.com>
2018-01-13 13:24:20 +0100

    register almost complete

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -19,8 +19,8 @@ class UserType extends AbstractType
             ->add('login', TextType::class)
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options' => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password')))
+                'first_options' => array('label' => 'Hasło: '),
+                'second_options' => array('label' => 'Powtórz hasło: ')))
             ->add('email', EmailType::class)
         ;
     }
